8a0995783056e2c7b88ba32e4dfcc2bda07b6ad5
Matheus Morais <ti.matheus.morais@gmail.com>
2022-11-28 23:02:41 +0000

    Role model and database created

---
+++ app/Models/Role.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    const FACTORY_ADMIN = 1;
+    const MANAGER = 2;
+    const DRIVER = 3;
+    const FACTORY_EMPLOYEE = 4;
+    const SHIPPING_EMPLOYEE = 5;
+    const SHIPPING_ADMIN = 6;
+
+    use HasFactory;
+}
